091675e194b933689d9da37b335f1e8b50f36c29
Baidewei <gerant9seminario@gmail.com>
2024-09-07 22:40:42 -0400

    Proteccion de ruta

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,6 +1,16 @@
 import clientPromise from '../../../../lib/mongodb';
 
 export async function GET(req) {
+  const apiKey = req.headers.get('db-api-key');
+  const validApiKey = process.env.MONGO_API_KEY;
+
+  if (!apiKey || apiKey !== validApiKey) {
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+      status: 401,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
   try {
     const client = await clientPromise;
     const db = client.db();
@@ -12,7 +22,7 @@ export async function GET(req) {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (e) {
-    return new Response(JSON.stringify({ error: 'Error al obtener los datos' }), {
+    return new Response(JSON.stringify({ error: 'Error fetching data' }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
     });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,35 @@
-//Componente que se renderiza (RSC = React Server Component)
-async function HomePage() {
-    return (
-        <div>
-            
-        </div>
-    )
+"use client";
+import { useEffect } from 'react';
+
+function HomePage() {
+  const fetchData = async () => {
+    try {
+      const res = await fetch('/api/data', {
+        headers: {
+          'db-api-key': process.env.NEXT_PUBLIC_MONGO_API_KEY, 
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status}`);
+      }
+
+      const result = await res.json();
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
+  return (
+    <div>
+      Soy el home
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
