04bc1f6c458e8e3f1a4df21669671e3c34228b4d
Baidewei <gerant9seminario@gmail.com>
2024-09-12 20:35:48 -0400

    Not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 export default function NotFound() {
     return <section>
-        <h1>404</h1>
         <p>Pagina no encontrada</p>
         <Link href="/">Volver</Link>
     </section>
